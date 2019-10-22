585c684270ee526eadbda62ea8fa61d3bde95036
bgauryy <bgauryy@gmail.com>
2019-10-22 13:54:56 +0300

    client.js - add DATA URI length

--- sample/puppeteer.js
+++ sample/puppeteer.js
@@ -4,34 +4,33 @@ const fs = require('fs');
 const path = require('path');
 
 (async () => {
-    const browser = await puppeteer.launch({args: ["--proxy-server='direct://'", '--proxy-bypass-list=*']})
+    const browser = await puppeteer.launch({
+        args: ['--disable-contextual-search', '–disable-translate', '--disable-headless-mode', "--proxy-server='direct://'", '--proxy-bypass-list=*',]
+    });
     const page = await Scanner.getSession(await browser.newPage(), {
         log: true,
         rules: {
             stealth: true,
             userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3933.0 Safari/537.36',
-            disableCSP: true,
-            adBlocking: true,
+            disableCSP: false,
+            adBlocking: false,
+            disableServices: false,
         },
         collect: {
             frames: true,
-
             scripts: true,
             scriptSource: true,
             scriptDOMEvents: true,
             scriptCoverage: true,
-
             styles: true,
             styleSource: true,
             styleCoverage: true,
-
             requests: true,
             responses: true,
             bodyResponse: [],
             dataURI: true,
-
+            websocket: true,
             serviceWorker: true,
-
             logs: true,
             console: true,
             errors: true,
@@ -43,7 +42,6 @@ const path = require('path');
 
     await page.goto('https://example.com', {waitUntil: 'load'});
     await sleep(10);
-
     const data = await page.getData();
     await browser.close();
     await saveData(data, path.resolve('result.json'));
