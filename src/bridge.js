19e6c3f6a6488ad9e03283626cd801770a0b0ab1
guy@perimeterx.com <guy@perimeterx.com>
2020-02-18 10:40:46 +0200

    bridge.js - add comment

--- src/bridge.js
+++ src/bridge.js
@@ -1,6 +1,11 @@
 const CRI = require('chrome-remote-interface');
 const url = require('url');
 
+/**
+ * @param page
+ * Puppeteer Page object or object with this structure: {host, port}
+ * @return {Promise<unknown>}
+ */
 async function getChromeClient(page) {
     if (page.hostname && page.port) {
         return await CRI({host: page.hostname, port: page.port});
