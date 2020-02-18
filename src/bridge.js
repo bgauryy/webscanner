219aa749686654d040d52f547867dcf1f4b5d610
guy@perimeterx.com <guy@perimeterx.com>
2020-02-18 10:39:22 +0200

    bridge.js - support direct execution

--- src/bridge.js
+++ src/bridge.js
@@ -2,9 +2,13 @@ const CRI = require('chrome-remote-interface');
 const url = require('url');
 
 async function getChromeClient(page) {
-    const connection = page._client._connection._url;
-    const {hostname, port} = url.parse(connection, true);
-    return await CRI({host: hostname, port});
+    if (page.hostname && page.port) {
+        return await CRI({host: page.hostname, port: page.port});
+    } else {
+        const connection = page._client._connection._url;
+        const {hostname, port} = url.parse(connection, true);
+        return await CRI({host: hostname, port});
+    }
 }
 
 module.exports = {
