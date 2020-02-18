b2276fccca6a25c18c031a4811cf7c6c8cdae2d4
guy@perimeterx.com <guy@perimeterx.com>
2020-02-18 10:54:50 +0200

    puppeteer.js

--- sample/puppeteer.js
+++ sample/puppeteer.js
@@ -1,21 +1,21 @@
 const puppeteer = require('puppeteer');
 const Scanner = require('../src/index');
-const URL = 'https://example.com';
+const URL = 'http://example.com';
 
 (async () => {
     const browser = await puppeteer.launch({
         args: ['--proxy-server="direct://"', '--proxy-bypass-list=*', '--disable-web-security']
     });
-    const page = await Scanner.getSession(await browser.newPage(), {
-        log: true,
-        rules: {
-            stealth: true,
-            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3933.0 Safari/537.36',
-            disableCSP: false,
-            adBlocking: false,
-            disableServices: false,
-        },
-        collect: {}
+    let page = await browser.newPage();
+
+    page = await Scanner.getSession(page, {
+        stealth: true,
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3933.0 Safari/537.36',
+        disableCSP: false,
+        adBlocking: false,
+        disableServices: false,
+        scripts: true,
+        content: true
     });
 
     await page.goto(URL, {waitUntil: 'load'});
