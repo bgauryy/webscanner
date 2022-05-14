95773fe1404f247a5d3065f2edf680470c23c3c3
GuyB <bgauryy@gmail.com>
2022-05-14 15:06:58 +0300

    add npmrc

---
+++ src/index.esm.js
@@ -0,0 +1,24 @@
+const Scanner = require('./scanner.js');
+
+const LOG = require('./logger.js');
+
+const {
+  getConfiguration
+} = require('./configuration.js');
+/**
+ *
+ * @param page - puppeteer page
+ * @param opts - scanning configuration
+ * @return {Promise}
+ */
+
+
+async function getSession(page, opts = {}) {
+  const configuration = getConfiguration(page, opts);
+  LOG.setEnabled(configuration.log);
+  return await Scanner.getSession(configuration);
+}
+
+module.exports = {
+  getSession
+};
